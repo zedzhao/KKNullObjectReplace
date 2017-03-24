22bae9b7a9d743afceeaaccf73ab72233670af47
zhaokun <zedzhao@gmail.com>
2017-03-24 10:06:52 +0800

    Initial Commit

---
+++ NullProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NullProject
+//
+//  Created by Kun on 24/03/2017.
+//  Copyright © 2017 Kun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
